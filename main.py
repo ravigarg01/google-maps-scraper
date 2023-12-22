de6893ef4f0431f6a6a1864505c88e9f34c5c006
Prashant Saraswat <135106502+cwprash@users.noreply.github.com>
2023-12-22 18:45:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from src.gmaps import Gmaps
 from fastapi import FastAPI
 from typing import List
@@ -6,37 +7,82 @@ from fastapi.middleware.cors import CORSMiddleware
 from amazon_paapi import AmazonApi
 from credentials import KEY, SECRET, TAG, COUNTRY
 
+
 class QueryModel(BaseModel):
     queries: List[str]
-    
+
+
 class AsinModel(BaseModel):
     asins: List[str]
-    
+
+
 amazon = AmazonApi(KEY, SECRET, TAG, COUNTRY)
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.post("/MapsSearch")
 async def search_places(query_model: QueryModel):
     result = Gmaps.places(query_model.queries)
     return result
 
-@app.get('/test')
-async def test():
-    return {'message': 'hello world'}
+# @app.post('/asinData')
+# async def asinData(asins: AsinModel):
+#     asins = asins.asins
+#     results = []
+
+#     tasks = [amazon.get_items(asin) for asin in asins]
+#     results = await asyncio.gather(*tasks)
+
+#     return results
+
 
 @app.post('/asinData')
 async def asinData(asins: AsinModel):
     asins = asins.asins
     results = []
-    for asin in asins:
-        result = amazon.get_items(asin)
-        results.append(result)
-    return results
+    try:
+
+        airtable_data = get_airtable_asin_title()
+        for asin in asins:
+            result = amazon.get_items(asin)
+            results.append(result)
+
+        items_table = []
+
+        for item_list in results:
+            for item in item_list:
+
+                asin = item.asin
+                title = item._item_info._title._display_value
+                image_url = item._images._primary._medium._url
+                detail_page_url = item._detail_page_url
+                # airtable_title = airtable_data[asin]
+                publication_date = item._item_info._content_info._publication_date._display_value
+                parsed_date = datetime.strptime(
+                    publication_date, "%Y-%m-%dT%H:%M:%S.%f%z")
+
+                formatted_date = parsed_date.strftime("%Y-%m-%d")
+
+                items_table.append({
+
+                    "ASIN": f'<a href="{detail_page_url}" target="_blank">{asin}</a>',
+                    "Title": title,
+                    "Image URL": f'<img src="{image_url}" alt="{title}" style="max-width:100px; max-height:100px;">',
+                    "Publication Date": formatted_date
+                })
+
+        return items_table
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+def get_airtable_asin_title():
+    pass

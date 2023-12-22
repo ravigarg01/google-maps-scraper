f76224d5ebe8e4e49ec6fb1790b9c74bbfd7e993
ravigarg01 <learnravi225@gmail.com>
2023-12-22 13:51:12 +0530

    amazon paapi

---
+++ credentials.py
@@ -0,0 +1,5 @@
+
+KEY = 'AKIAIH2INWASV3ZQPCWQ'
+SECRET = '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'
+TAG = 'rahulaggarw0c-21'
+COUNTRY = 'IN'
--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from fastapi import FastAPI
 from typing import List
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from amazon_paapi import AmazonApi
+from credentials import KEY, SECRET, TAG, COUNTRY
 
 class QueryModel(BaseModel):
     queries: List[str]
+    
+class AsinModel(BaseModel):
+    asins: List[str]
+    
+amazon = AmazonApi(KEY, SECRET, TAG, COUNTRY)
 
 app = FastAPI()
 app.add_middleware(
@@ -24,3 +31,12 @@ async def search_places(query_model: QueryModel):
 @app.get('/test')
 async def test():
     return {'message': 'hello world'}
+
+@app.post('/asinData')
+async def asinData(asins: AsinModel):
+    asins = asins.asins
+    results = []
+    for asin in asins:
+        result = amazon.get_items(asin)
+        results.append(result)
+    return results

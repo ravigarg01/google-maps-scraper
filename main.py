e4cf1e5e2bb3fc88c0fa8040301b5a8a8d49256c
Prashant Saraswat <135106502+cwprash@users.noreply.github.com>
2023-12-23 12:37:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from credentials import KEY, SECRET, TAG, COUNTRY, Airtable_api_token
 from pyairtable import Api
 
 
-
 class QueryModel(BaseModel):
     queries: List[str]
 
@@ -49,9 +48,9 @@ async def search_places(query_model: QueryModel):
 @app.post('/asinData')
 async def asinData(asins: AsinModel):
     asins = asins.asins
-    final_results = amazon.get_items(items = asins)
+    final_results = amazon.get_items(items=asins)
     return final_results
-    
+
 
 @app.get('/getExamcart')
 async def getExamcart():
@@ -59,11 +58,69 @@ async def getExamcart():
     records = airtable.all()
     return records
 
+
 def getAirtable():
     airtable = Api(api_key=Airtable_api_token)
     return airtable
+
 
 def getExamcartAirtable():
     airtable = getAirtable()
     examcart = airtable.table('apph7VmzXLax4KGj1', 'Examcart 2023')
     return examcart
+
+
+@app.post('/asin')
+async def asinData(asins: AsinModel):
+    asins = asins.asins
+    results = []
+    try:
+
+        airtable_data = get_airtable_asin_title()
+        for asin in asins:
+            result = amazon.get_items(asin)
+            results.append(result)
+
+        items_table1 = []
+        items_table2 = []
+
+        for item_list in results:
+            for item in item_list:
+
+                asin = item.asin
+                title = item._item_info._title._display_value
+                image_url = item._images._primary._medium._url
+                detail_page_url = item._detail_page_url
+                # airtable_title = airtable_data[asin]
+                publication_date = item._item_info._content_info._publication_date._display_value
+                parsed_date = datetime.strptime(
+                    publication_date, "%Y-%m-%dT%H:%M:%S.%f%z")
+
+                formatted_date = parsed_date.strftime("%Y-%m-%d")
+
+                items_table1.append({
+
+                    "ASIN": f'<a href="{detail_page_url}" target="_blank">{asin}</a>',
+                    "Title": title,
+                    "Image URL": f'<img src="{image_url}" alt="{title}" style="max-width:100px; max-height:100px;">',
+                    "Publication Date": formatted_date
+                })
+                items_table2.append({
+
+                    "ASIN": f'<a href="{detail_page_url}" target="_blank">{asin}</a>',
+                    "Title": title,
+                    "Image URL": f'<img src="{image_url}" alt="{title}" style="max-width:100px; max-height:100px;">',
+                    "Publication Date": formatted_date
+                })
+
+        return {
+            "table1": items_table1,
+            "table2": items_table2
+
+        }
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+def get_airtable_asin_title():
+    pass

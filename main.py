e46f7af3d642cb9757b50c64fc950794c5b74c20
Prashant Saraswat <135106502+cwprash@users.noreply.github.com>
2023-12-23 18:20:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,26 +34,15 @@ async def search_places(query_model: QueryModel):
     result = Gmaps.places(query_model.queries)
     return result
 
+
 # @app.post('/asinData')
 # async def asinData(asins: AsinModel):
 #     asins = asins.asins
-#     results = []
-
-#     tasks = [amazon.get_items(asin) for asin in asins]
-#     results = await asyncio.gather(*tasks)
-
-#     return results
+#     final_results = amazon.get_items(items=asins)
+#     return final_results
 
 
-@app.post('/asinData')
-async def asinData(asins: AsinModel):
-    asins = asins.asins
-    final_results = amazon.get_items(items=asins)
-    return final_results
-
-
-@app.get('/getExamcart')
-async def getExamcart():
+def getExamcart():
     airtable = getExamcartAirtable()
     records = airtable.all()
     return records
@@ -73,49 +62,75 @@ def getExamcartAirtable():
 @app.post('/asin')
 async def asinData(asins: AsinModel):
     asins = asins.asins
-    results = []
+    airtable_data = getExamcart()
+    items_table1 = []
+    items_table2 = []
+    items_table3 = []
+    asin_details = {}
+
     try:
+        for entry in airtable_data:
+            asinId = entry["fields"].get("ASIN", None)
+            ecommerce_title = entry["fields"].get("E-commerce Title", None)
+            seller_sku = entry["fields"].get("Seller SKU", None)
 
-        airtable_data = get_airtable_asin_title()
-        for asin in asins:
-            result = amazon.get_items(asin)
-            results.append(result)
+            if asinId in asins:
+                asin_details[asinId] = {
+                    "title": ecommerce_title, "sku": seller_sku}
 
-        items_table1 = []
-        items_table2 = []
+        final_results = amazon.get_items(items=asins)
 
-        for item_list in results:
-            for item in item_list:
+        for item in final_results:
 
-                asin = item.asin
-                title = item._item_info._title._display_value
-                image_url = item._images._primary._medium._url
-                detail_page_url = item._detail_page_url
-                # airtable_title = airtable_data[asin]
-                publication_date = item._item_info._content_info._publication_date._display_value
-                parsed_date = datetime.strptime(
-                    publication_date, "%Y-%m-%dT%H:%M:%S.%f%z")
+            asin = item.asin
+            title = item._item_info._title._display_value
+            image_url = item._images._primary._medium._url
+            detail_page_url = item._detail_page_url
+            # airtable_title = airtable_data[asin]
+            publication_date = item._item_info._content_info._publication_date._display_value
+            parsed_date = datetime.strptime(
+                publication_date, "%Y-%m-%dT%H:%M:%S.%f%z")
 
-                formatted_date = parsed_date.strftime("%Y-%m-%d")
+            formatted_date = parsed_date.strftime("%Y-%m-%d")
+            details = asin_details[asin]
+            airtable_title = details.get("title", "Title not available")
+            airtable_sku = details.get("sku", "SKU not available")
 
-                items_table1.append({
+            items_table1.append({
 
-                    "ASIN": f'<a href="{detail_page_url}" target="_blank">{asin}</a>',
-                    "Title": title,
-                    "Image URL": f'<img src="{image_url}" alt="{title}" style="max-width:100px; max-height:100px;">',
-                    "Publication Date": formatted_date
-                })
-                items_table2.append({
+                "ASIN": f'<a href="{detail_page_url}" target="_blank">{asin}</a>',
+                "SKU": airtable_sku,
+                "Title": title,
+                "Image URL": f'<img src="{image_url}" alt="{title}" style="max-width:100px; max-height:100px;">',
+                "Publication Date": formatted_date,
+                "Airtable Title": airtable_title,
+                "Title Maching": title == airtable_title
+            })
+            items_table2.append({
 
-                    "ASIN": f'<a href="{detail_page_url}" target="_blank">{asin}</a>',
-                    "Title": title,
-                    "Image URL": f'<img src="{image_url}" alt="{title}" style="max-width:100px; max-height:100px;">',
-                    "Publication Date": formatted_date
-                })
+                "ASIN": f'<a href="{detail_page_url}" target="_blank">{asin}</a>',
+                "SKU": airtable_sku,
+                "Title": title,
+                "Image URL": f'<img src="{image_url}" alt="{title}" style="max-width:100px; max-height:100px;">',
+                "Publication Date": formatted_date,
+                "Airtable Title": airtable_title,
+                "Title Maching": title == airtable_title
+            })
+            items_table3.append({
+
+                "ASIN": f'<a href="{detail_page_url}" target="_blank">{asin}</a>',
+                "SKU": airtable_sku,
+                "Title": title,
+                "Image URL": f'<img src="{image_url}" alt="{title}" style="max-width:100px; max-height:100px;">',
+                "Publication Date": formatted_date,
+                "Airtable Title": airtable_title,
+                "Title Maching": title == airtable_title
+            })
 
         return {
-            "table1": items_table1,
-            "table2": items_table2
+            "title": items_table1,
+            "category": items_table2,
+            "suppress": items_table3
 
         }
     except Exception as e:

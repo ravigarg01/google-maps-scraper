f56364c41dc4ceb512e320a785e283dec61143e6
Prashant Saraswat <135106502+cwprash@users.noreply.github.com>
2024-02-02 18:00:34 +0530

    update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+import uvicorn
 from src.gmaps import Gmaps
 from fastapi import FastAPI
 from typing import List
@@ -8,13 +10,13 @@ from fastapi.middleware.cors import CORSMiddleware
 # from credentials import KEY, SECRET, TAG, COUNTRY, Airtable_api_token
 # from pyairtable import Api
 # amazon = AmazonApi(KEY, SECRET, TAG, COUNTRY)
-
-
-class QueryModel(BaseModel):
-    queries: List[str]
+import time
+import requests
 
 
 app = FastAPI()
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -23,6 +25,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+ydcurl = 'http://164.52.208.218:8000'
+second_endpoint = f"{ydcurl}/placesapi/store_places/"
+
+
+class QueryModel(BaseModel):
+    queries: List[str]
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
 
 @app.post("/MapsSearch")
 async def search_places(query_model: QueryModel):
@@ -30,11 +44,128 @@ async def search_places(query_model: QueryModel):
     return result
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+async def places(query_list):
+    result = Gmaps.places(query_list)
+    return result
 
-###################################################################################################
+
+async def update_status(query_city_id, status):
+
+    try:
+        status_url = f"{ydcurl}/placesapi/update_query_id_status/{query_city_id}/{status}/"
+        response = requests.get(status_url)
+
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+
+    except Exception as e:
+        print(f"An unexpected error occurred while updating final status: {e}")
+        return False
+
+
+async def map_search():
+
+    try:
+
+        get_Query_url = f"{ydcurl}/placesapi/get_query_city_by_status/Pending/"
+        get_Query_response = requests.get(get_Query_url)
+        get_Query_response.raise_for_status()
+
+        if get_Query_response.status_code == 200:
+            get_Query_response_data = get_Query_response.json()
+            query = get_Query_response_data.get("query", "")
+            query_city_id = get_Query_response_data.get(
+                "query_city_id", "")
+
+            if query is None:
+                time.sleep(1800)
+            else:
+                # await update_status(query_city_id, "InProcess")
+                # update_status_url = f"{ydcurl}/placesapi/update_query_id_status/{query_city_id}/InProcess/"
+                # requests.get(update_status_url)
+                # Create a list with a single element, which is your query
+                body = [query]
+
+                print(body)
+                response_data = await places(body)
+
+                if len(response_data) < 1:
+                    await update_status(query_city_id, "InProcess")
+                    # update_status_url = f"{ydcurl}/placesapi/update_query_id_status/{query_city_id}/Cancelled/"
+                    # requests.get(update_status_url)
+                    time.sleep(600)
+                else:
+                    post_body = []
+                    for place in response_data:
+                        google_place_id = place.get("place_id", "")
+                        name = place.get("name", "")
+                        link = place.get("link", "")
+                        phone_number = place.get("phone", "")
+                        website = place.get("website", "")
+                        detailed_address = place.get(
+                            "detailed_address", {})
+                        ward = detailed_address.get("ward", "")
+                        state = detailed_address.get("state", "")
+                        city = detailed_address.get("city", "")
+                        postal_code = detailed_address.get(
+                            "postal_code", "")
+                        street = detailed_address.get("street", "")
+                        status = 3
+
+                        query_id = {"query_city_id": query_city_id}
+                        competitors = [
+                            {
+                                "competetor_place_id": None,
+                                "competetor_name": competitor.get("name", ""),
+                                "competetor_link": competitor.get("link", "")
+                            }
+                            for competitor in place.get("competitors", [])
+                        ]
+
+                        post_body.append({
+                            "google_place_id": google_place_id,
+                            "name": name,
+                            "link": link,
+                            "phone_number": phone_number,
+                            "website": website,
+                            "ward": ward,
+                            "state": state,
+                            "city": city,
+                            "postal_code": postal_code,
+                            "street": street,
+                            "status": status,
+                            "query_city_id": query_id,
+                            "competitors": competitors
+                        })
+
+                    post_response = requests.post(
+                        second_endpoint, json=post_body)
+                    post_response.raise_for_status()
+
+                    if post_response.status_code == 201:
+                        await update_status(query_city_id, "Completed")
+                        # final_status_url = f"{ydcurl}/placesapi/update_query_id_status/{query_city_id}/Completed/"
+                        # requests.get(final_status_url)
+
+    except requests.RequestException as req_error:
+        print(f"Request error: {req_error}")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+async def on_startup():
+    # Call map_search at the start of the application
+    await map_search()
+
+# Register the on_startup event handler
+app.add_event_handler("startup", on_startup)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 ###########################################################################################################################################
